062ab5fdb8747805898d0d86ee975dc284a924d4
jbrent <jessica.a.brent@gmail.com>
2023-04-04 18:15:34 -0400

    add datacenter/world section and save state

---
+++ src/datacenters.js
@@ -0,0 +1,59 @@
+const datacenters = [
+  {
+    name: 'Aether',
+    locale: 'NA',
+    worlds: [ 'Adamantoise', 'Cactaur', 'Faerie', 'Gilgamesh', 'Jenova', 'Midgardsormr', 'Sargatanas', 'Siren', ],
+  },
+  {
+    name: 'Chaos',
+    locale: 'EU',
+    worlds: [ 'Cerberus', 'Louisoix', 'Moogle', 'Omega', 'Phantom', 'Ragnarok', 'Sagittarius', 'Spriggan', ],
+  },
+  {
+    name: 'Crystal',
+    locale: 'NA',
+    worlds: [ 'Balmung', 'Brynhildr', 'Coeurl', 'Diabolos', 'Goblin', 'Malboro', 'Mateus', 'Zalera', ],
+  },
+  {
+    name: 'Dynamis',
+    locale: 'NA',
+    worlds: [ 'Halicarnassus', 'Maduin', 'Marilith', 'Seraph', ],
+  },
+  {
+    name: 'Elemental',
+    locale: 'JP',
+    worlds: [ 'Aegis', 'Atomos', 'Carbuncle', 'Garuda', 'Gungnir', 'Kujata', 'Tonberry', 'Typhon', ],
+  },
+  {
+    name: 'Gaia',
+    locale: 'JP',
+    worlds: [ 'Alexander', 'Bahamut', 'Durandal', 'Fenrir', 'Ifrit', 'Ridill', 'Tiamat', 'Ultima', ],
+  },
+  {
+    name: 'Light',
+    locale: 'EU',
+    worlds: [ 'Alpha', 'Lich', 'Odin', 'Phoenix', 'Raiden', 'Shiva', 'Twintania', 'Zodiark', ],
+  },
+  {
+    name: 'Mana',
+    locale: 'JP',
+    worlds: [ 'Anima', 'Asura', 'Chocobo', 'Hades', 'Ixion', 'Masamune', 'Pandaemonium', 'Titan', ],
+  },
+  {
+    name: 'Materia',
+    locale: 'OC',
+    worlds: [ 'Bismarck', 'Ravana', 'Sephirot', 'Sophia', 'Zurvan', ],
+  },
+  {
+    name: 'Meteor',
+    locale: 'JP',
+    worlds: [ 'Belias', 'Mandragora', 'Ramuh', 'Shinryu', 'Unicorn', 'Valefor', 'Yojimbo', 'Zeromus', ],
+  },
+  {
+    name: 'Primal',
+    locale: 'NA',
+    worlds: [ 'Behemoth', 'Excalibur', 'Exodus', 'Famfrit', 'Hyperion', 'Lamia', 'Leviathan', 'Ultros' ],
+  },
+];
+
+module.exports = { datacenters };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-const { FlexLayout, QMainWindow, QCheckBox, QTextEdit, QPushButton, QApplication, QClipboardMode, QWidget, QLabel } = require('@nodegui/nodegui');
-
+const { FlexLayout, QMainWindow, QCheckBox, QTextEdit, QPushButton, QApplication, QClipboardMode, QWidget, QLabel, QComboBox, QSettings } = require('@nodegui/nodegui');
+const { datacenters } = require('./datacenters');
 
 const chatOptions = [
-    { short: 's', label: 'Say', checked: false },
-    { short: 'y', label: 'Yell', checked: false },
-    { short: 'sh', label: 'Shout', checked: false },
-    { short: 'p', label: 'Party', checked: false },
-    { short: 'fc', label: 'Free Company', checked: false },
-    { short: 'l1', label: 'Linkshell 1', checked: false },
-    { short: 'l2', label: 'Linkshell 2', checked: false },
-    { short: 'l3', label: 'Linkshell 3', checked: false },
-    { short: 'l4', label: 'Linkshell 4', checked: false },
-    { short: 'l5', label: 'Linkshell 5', checked: false },
-    { short: 'l6', label: 'Linkshell 6', checked: false },
-    { short: 'l7', label: 'Linkshell 7', checked: false },
-    { short: 'l8', label: 'Linkshell 8', checked: false },
-    { short: 'cwl1', label: 'Cross-world Linkshell 1', checked: false },
-    { short: 'cwl2', label: 'Cross-world Linkshell 2', checked: false },
-    { short: 'cwl3', label: 'Cross-world Linkshell 3', checked: false },
-    { short: 'cwl4', label: 'Cross-world Linkshell 4', checked: false },
-    { short: 'cwl5', label: 'Cross-world Linkshell 5', checked: false },
-    { short: 'cwl6', label: 'Cross-world Linkshell 6', checked: false },
-    { short: 'cwl7', label: 'Cross-world Linkshell 7', checked: false },
-    { short: 'cwl8', label: 'Cross-world Linkshell 8', checked: false },
+    { short: 's', label: 'Say', },
+    { short: 'y', label: 'Yell', },
+    { short: 'sh', label: 'Shout', },
+    { short: 'p', label: 'Party', },
+    { short: 'fc', label: 'Free Company', },
+    { short: 'l1', label: 'Linkshell 1', },
+    { short: 'l2', label: 'Linkshell 2', },
+    { short: 'l3', label: 'Linkshell 3', },
+    { short: 'l4', label: 'Linkshell 4', },
+    { short: 'l5', label: 'Linkshell 5', },
+    { short: 'l6', label: 'Linkshell 6', },
+    { short: 'l7', label: 'Linkshell 7', },
+    { short: 'l8', label: 'Linkshell 8', },
+    { short: 'cwl1', label: 'Cross-world Linkshell 1', },
+    { short: 'cwl2', label: 'Cross-world Linkshell 2', },
+    { short: 'cwl3', label: 'Cross-world Linkshell 3', },
+    { short: 'cwl4', label: 'Cross-world Linkshell 4', },
+    { short: 'cwl5', label: 'Cross-world Linkshell 5', },
+    { short: 'cwl6', label: 'Cross-world Linkshell 6', },
+    { short: 'cwl7', label: 'Cross-world Linkshell 7', },
+    { short: 'cwl8', label: 'Cross-world Linkshell 8', },
 ];
+
+const settings = new QSettings('jbrent', 'relay-gen');
 
 const win = new QMainWindow();
 win.setWindowTitle('FFXIV Relay Macro Generator')
@@ -65,6 +67,10 @@ for (option of chatOptions) {
     const checkbox = new QCheckBox();
     checkbox.setText(option.label);
     checkbox.setObjectName(option.short);
+    checkbox.setChecked(settings.value(option.short).toBool() ?? false);
+    checkbox.addEventListener('clicked', (checked) => {
+      settings.setValue(checkbox.objectName(), checked);
+    });
 
     if (option.short.startsWith('l')) {
         lsChatCheckBoxViewLayout.addWidget(checkbox);
@@ -74,6 +80,49 @@ for (option of chatOptions) {
         mainChatCheckBoxViewLayout.addWidget(checkbox);
     }
 }
+
+const worldBoxView = new QWidget();
+const worldBoxViewLayout = new FlexLayout();
+worldBoxView.setObjectName('worldBoxView');
+worldBoxView.setLayout(worldBoxViewLayout);
+
+const worldSelectLabel = new QLabel();
+worldSelectLabel.setText('Optional: \nSelect world and use \'%w\'\nas a placeholder.');
+worldSelectLabel.setObjectName('worldSelectLabel');
+
+const dataCenterDropdown = new QComboBox();
+dataCenterDropdown.setObjectName('dataCenterDropdown');
+
+const worldSelectDropdown = new QComboBox();
+worldSelectDropdown.setObjectName('worldSelectDropdown');
+
+for (dc of datacenters) {
+  dataCenterDropdown.addItem(undefined, dc.name);
+}
+
+dataCenterDropdown.addEventListener('currentTextChanged', async (text) => {
+  worldSelectDropdown.clear();
+  for (dc of datacenters) {
+    if (dc.name === text) {
+      settings.setValue('lastDc', dc.name);
+      for (world of dc.worlds) {
+        worldSelectDropdown.addItem(undefined, world);
+      }
+    }
+  }
+});
+
+dataCenterDropdown.setCurrentText(settings.value('lastDc').toString() ?? '');
+worldSelectDropdown.setCurrentText(settings.value('lastWorld').toString() ?? '');
+
+worldSelectDropdown.addEventListener('currentTextChanged', async (text) => {
+  settings.setValue('lastWorld', text);
+});
+
+worldBoxViewLayout.addWidget(worldSelectLabel);
+worldBoxViewLayout.addWidget(dataCenterDropdown);
+worldBoxViewLayout.addWidget(worldSelectDropdown);
+mainChatCheckBoxViewLayout.addWidget(worldBoxView);
 
 const macroBoxView = new QWidget();
 const macroBoxViewLayout = new FlexLayout();
@@ -87,7 +136,8 @@ inputBoxView.setLayout(inputBoxViewLayout);
 
 const inputTextEdit = new QTextEdit();
 inputTextEdit.setObjectName('inputTextEdit');
-inputTextEdit.setPlaceholderText('Type something here and click Generate!');
+const lastInputText = settings.value('inputText').toString();
+lastInputText ? inputTextEdit.setText(lastInputText) : inputTextEdit.setPlaceholderText('Type something here and click Generate!');
 
 const generateButton = new QPushButton();
 generateButton.setObjectName('generateButton');
@@ -109,10 +159,13 @@ copyButton.setText('Copy Macro');
 
 generateButton.addEventListener('clicked', () => {
     const children = [...mainChatCheckBoxView.children(), ...lsChatCheckBoxView.children(), ...cwlsChatCheckBoxView.children()];
+    const inputText = inputTextEdit.toPlainText();
     let outputText = '';
+
+    settings.setValue('inputText', inputText);
     for (child of children) {
         if (child instanceof QCheckBox && child.isChecked()) {
-            outputText = outputText + `/${child.objectName()} ${inputTextEdit.toPlainText()}\n`
+            outputText = outputText + `/${child.objectName()} ${inputText.replace(/%w/g, worldSelectDropdown.currentText())}\n`
         }
     }
     outputTextEdit.setPlainText(outputText);
@@ -176,6 +229,18 @@ const rootStyleSheet = `
     padding: 2em;
     margin-bottom: 2em;
     flex-direction: column;
+  }
+  #worldBoxView {
+    width: 150em;
+    padding: 2em;
+    margin-top: 5em;
+    margin-bottom: 2em;
+    flex-direction: column;
+    align-content: flex-end;
+    justify-content: flex-end;
+  }
+  #worldSelectLabel {
+    width: 150em;
   }
   #inputTextEdit, #outputTextEdit {
     margin-right: 0.2em;
